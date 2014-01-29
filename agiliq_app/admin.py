64707ca36bc3c9ae5add63fd9e28f6d78f3f6564
manjunath24 <hugar.manjunathrt@gmail.com>
2014-01-29 13:01:35 +0530

    initial master commit

---
+++ agiliq_app/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from agiliq_app.models import Article,Comment
+
+admin.site.register(Article)
+admin.site.register(Comment)
+
+# Register your models here.
---
+++ agiliq_app/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from taggit.managers import TaggableManager
+# Create your models here.
+
+
+STATUS = (
+    ('1', 'Active'),
+    ('0', 'Inactive'),
+    
+)
+
+class Article(models.Model):
+	title = models.CharField(max_length=200)
+	body = models.TextField()
+	blog_file = models.FileField(upload_to='images', blank = True)
+	created_by = models.CharField(max_length=100)
+	pub_date = models.DateTimeField('date published')
+	tags = TaggableManager()
+	
+	def __unicode__(self):
+		return self.title
+
+class Comment(models.Model):
+	blogid = models.IntegerField()
+	name = models.CharField(max_length=100)
+	email = models.CharField(max_length=100)
+	dessription = models.TextField()
+	status = models.CharField(max_length=1,choices=STATUS,default=0)
+	def __unicode__(self):
+		return self.name
