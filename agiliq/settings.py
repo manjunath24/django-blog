fed5856f70a2952ce93a65f824b1b02d9b4c89d6
manjunath24 <hugar.manjunathrt@gmail.com>
2014-01-29 18:01:28 +0530

    file commited

--- agiliq/settings.py
+++ agiliq/settings.py
@@ -81,7 +81,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-MEDIA_ROOT = 'agiliq/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
