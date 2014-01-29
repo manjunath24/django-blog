3cfe8da741fd7cff24da35230b81ee6ce0212ef5
manjunath24 <hugar.manjunathrt@gmail.com>
2014-01-29 15:16:28 +0530

    First commit

---
+++ agiliq_app/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from agiliq_app.models import Article,Comment
+from agiliq_app.forms import CommentForm
+# Create your views here.
+
+def home(request):
+	data = Article.objects.all().order_by('-pub_date')
+	return render(request,'index.html',{'data':data})
+
+
+def detail(request,blog_id=None):
+	
+	data = Article.objects.filter(pk=blog_id)
+	get_comments = Comment.objects.filter(blogid=blog_id,status=1)
+
+	if request.method == 'POST':
+		form = CommentForm(request.POST)
+		if form.is_valid():
+			name = request.POST['name']
+			email = request.POST['email']
+			description = request.POST['description']
+			comment = Comment(blogid=blog_id,name=name,email=email,dessription=description,status=0)
+			comment.save()
+			form = CommentForm()
+			return render(request,'detail.html',{'data':data,'form':form,'added':'success','comments':get_comments})
+	else:
+		form = CommentForm()
+	return render(request,'detail.html',{'data':data,'form':form,'comments':get_comments})	
+
