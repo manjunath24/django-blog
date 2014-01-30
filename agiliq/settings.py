c4d8acecac732e29aefc26e27b8c45f2473b247e
manjunath24 <hugar.manjunathrt@gmail.com>
2014-01-30 17:16:50 +0530

    bootstrap recommit

--- agiliq/settings.py
+++ agiliq/settings.py
@@ -93,13 +93,13 @@ STATICFILES_DIRS = (
 
 )
 
-'''
+
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-'''
+
 
 
 
