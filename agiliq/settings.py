725308882732d8f399a99864b09d0dbdcc16cd41
manjunath24 <hugar.manjunathrt@gmail.com>
2014-01-30 15:31:17 +0530

    new commit

--- agiliq/settings.py
+++ agiliq/settings.py
@@ -85,7 +85,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_ROOT= os.path.join(BASE_DIR,'static/')
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
