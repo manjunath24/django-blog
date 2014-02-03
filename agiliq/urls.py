40b1405f2f3292411e58c219198ca0dabf976cc5
manjunath24 <hugar.manjunathrt@gmail.com>
2014-02-03 12:11:48 +0530

    agiliq/urls.py changed

--- agiliq/urls.py
+++ agiliq/urls.py
@@ -9,7 +9,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'agiliq_app.views.home', name='home'),
-    url(r'^detail/(?P<blog_id>[^/]+)', 'agiliq_app.views.detail', name='details'),
-    url(r'^about', 'agiliq_app.views.about', name='about'),
+    url(r'^', include('agiliq_app.urls')),
 )
