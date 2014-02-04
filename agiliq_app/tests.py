a19e8fc9f939be0d14c8cd85262e2be87dc1c90c
manjunath24 <hugar.manjunathrt@gmail.com>
2014-02-04 15:44:30 +0530

    test case added

--- agiliq_app/tests.py
+++ agiliq_app/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class BlogViewsTestCase(TestCase):
+    def test_index(self):
+        resp = self.client.get('/')
+        self.assertEqual(resp.status_code, 200)
