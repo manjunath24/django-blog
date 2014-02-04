b77d3f22e2673b5b98c79fae0f711194e862f699
manjunath24 <hugar.manjunathrt@gmail.com>
2014-02-04 16:08:52 +0530

    added setup for Client

--- agiliq_app/tests.py
+++ agiliq_app/tests.py
@@ -1,8 +1,14 @@
 from django.test import TestCase
-
+from django.test import Client
 # Create your tests here.
 
 class BlogViewsTestCase(TestCase):
+
+    def setup(self):
+        self.client = Client()
+
     def test_index(self):
         resp = self.client.get('/')
         self.assertEqual(resp.status_code, 200)
+
+    
